95ebaa57266e6842744f1403ffb33016fbb96b1e
saban942025-cyber <saban942025@gmail.com>
2026-02-07 06:14:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-module.exports = nextConfig
+  webpack: (config) => {
+    config.externals.push({ canvas: 'commonjs canvas' });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
