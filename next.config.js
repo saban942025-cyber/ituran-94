020f635c5ca438f32f3d100889058439870dce55
saban942025-cyber <saban942025@gmail.com>
2026-02-07 06:29:10 +0200

    Update next.config.js
    npm install undici@latest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // פותר את שגיאת ה-undici ו-firebase storage
+  transpilePackages: ['undici', 'firebase', '@firebase/storage'],
+  
   webpack: (config) => {
+    // פותר את שגיאת ה-canvas ב-fabric
     config.externals.push({ canvas: 'commonjs canvas' });
     return config;
   },
